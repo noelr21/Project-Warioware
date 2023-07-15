894a4e9f33776ab1c30d8a99cf5e3a8067b2d55f
Noel Ramirez <rnoel2125@gmail.com>
2023-07-15 00:44:08 -0400

    Got enemy tracking up and running again with a raycast to check if player is occluded or not. This feature still requires testing. Furthermore a noticable lag between the player entering the detection circle and the enemy moving towards the player is present; no current leads for what could be causing this issue.

--- Scenes/Hitboxes and Hurtboxes/PlayerDetectionZone/PlayerDetectionZone.gd
+++ Scenes/Hitboxes and Hurtboxes/PlayerDetectionZone/PlayerDetectionZone.gd
@@ -18,14 +18,15 @@ func _on_PlayerDetectionZone_body_exited(_body:Node):
 
 #if player enters the detection zone check to see if they are in line of sight and if found do not forget them as
 #as long as they are in the detection zone
-func _physics_process(delta):
+func _physics_process(_delta):
 	if player != null && !playerFound:
 		var space_state = get_world_2d().direct_space_state
-		var query = PhysicsRayQueryParameters2D.create(self.position, player.position)
+		var query = PhysicsRayQueryParameters2D.create(self.global_position, player.position)
 		var result = space_state.intersect_ray(query)
 		#if no result leave
-		if result == null:
+		if result == null || result.is_empty():
 			return
+		print("result: ", result)
 		print("Object hit in raycast: ")
 		print(result.collider)
 		if result.collider == player:
